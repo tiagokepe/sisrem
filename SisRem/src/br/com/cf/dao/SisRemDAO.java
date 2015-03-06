9178c471b7e196b0f3977d354579c30e075e1ea8
giovanni.mori@ifpr.edu.br <giovanni.mori@ifpr.edu.br@11d0db24-5458-aeb8-0225-39bd4d45c791>
2015-03-06 19:16:35 +0000

    Criado o Controlador para operações comuns do Sistema, DAO para operações de autenticação.

---
+++ SisRem/src/br/com/cf/dao/SisRemDAO.java
@@ -0,0 +1,45 @@
+package br.com.cf.dao;
+
+import java.security.NoSuchAlgorithmException;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import br.com.cf.entity.Usuario;
+import br.com.cf.util.Encripty;
+
+public class SisRemDAO extends DAO {
+
+	private static SisRemDAO instance;
+
+	public static SisRemDAO getInstance() {
+		if (instance == null) {
+			instance = new SisRemDAO();
+		}
+		return instance;
+	}
+
+	public Usuario autentica(Usuario usuario) throws NoSuchAlgorithmException,
+			SQLException {
+
+		Usuario u = null;
+		String sql = "SELECT u.login, u.senha, p.nome FROM "
+				+ "comum.usuario u "
+				+ "INNER JOIN comum.pessoa p ON u.id_pessoa = p.id_pessoa "
+				+ "WHERE u.login = ? AND u.senha = ?";
+
+		PreparedStatement pstm = JDBC.getConnection().prepareStatement(sql);
+
+		pstm.setString(1, usuario.getLogin().toLowerCase());
+		pstm.setString(2, Encripty.criptografaSenha(usuario.getSenha()));
+		ResultSet rs = pstm.executeQuery();
+		while (rs.next()) {
+			u = new Usuario();
+			u.setLogin(rs.getString("login"));
+			u.setSenha(rs.getString("senha"));
+			u.setNome(rs.getString("nome"));
+		}
+		return u;
+	}
+
+}
--- SisRem/src/br/com/cf/dao/UsuarioDAO.java
+++ SisRem/src/br/com/cf/dao/UsuarioDAO.java
@@ -1,13 +1,5 @@
 package br.com.cf.dao;
 
-import java.security.NoSuchAlgorithmException;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-
-import br.com.cf.util.Encripty;
-import br.com.cf.entity.Usuario;
-
 public class UsuarioDAO extends DAO {
 
 	private static UsuarioDAO instance;
@@ -19,26 +11,4 @@ public class UsuarioDAO extends DAO {
 		return instance;
 	}
 
-	public Usuario autentica(Usuario usuario) throws NoSuchAlgorithmException,
-			SQLException {
-
-		Usuario u = null;
-		String sql = "SELECT u.login, u.senha, p.nome FROM "
-				 + "comum.usuario u "
-				 + "INNER JOIN comum.pessoa p ON u.id_pessoa = p.id_pessoa "
-				 + "WHERE u.login = ? AND u.senha = ?";
-		
-		PreparedStatement pstm = JDBC.getConnection().prepareStatement(sql);
-		
-		pstm.setString(1, usuario.getLogin().toLowerCase());
-		pstm.setString(2, Encripty.criptografaSenha(usuario.getSenha()));
-		ResultSet rs = pstm.executeQuery();
-		while (rs.next()) {
-			u = new Usuario();
-			u.setLogin(rs.getString("login"));
-			u.setSenha(rs.getString("senha"));
-			u.setNome(rs.getString("nome"));
-		}
-		return u;
-	}
 }
